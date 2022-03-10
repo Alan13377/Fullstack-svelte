5ff13df16eb1ca8b539613b034cd882b62595284
Alan <john1337@gmail.com>
2022-03-09 18:04:36 -0600

    Componentes

---
+++ src/lib/todo-svelte.svelte
@@ -0,0 +1,15 @@
+<div>
+	<form action="">
+		<input type="hidden" name="done" value="" />
+		<button aria-label="Marca completado/no completado">Don/NotDone</button>
+	</form>
+
+	<form action="">
+		<input type="text" />
+		<button aria-label="Guardar Tarea">Guardar</button>
+	</form>
+
+	<form action="">
+		<button aria-label="Borrar Tarea">Borrar</button>
+	</form>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+	import TodoSvelte from '../lib/todo-svelte.svelte';
+	const title = 'Todos';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
+
+<form action="" method="">
+	<input type="text" name="text" aria-label="Agregar tarea" placeholder="+ Agregra Tarea" />
+</form>
+
+<TodoSvelte />
+<TodoSvelte />
+<TodoSvelte />
